ed6a6665b0fa9e6fd8851d7b1f3fcc0f673ffb65
jameskenyon <james.kenyon08@gmail.com>
2020-06-27 16:14:09 +0100

    Initial xcode project setup.

---
+++ ios/Elenco/Elenco/View/MyListView.swift
@@ -0,0 +1,21 @@
+//
+//  MyListView.swift
+//  Elenco
+//
+//  Created by James Kenyon on 27/06/2020.
+//  Copyright © 2020 Elenco. All rights reserved.
+//
+
+import SwiftUI
+
+struct MyListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyListView()
+    }
+}
