56c64f94897772680b3cf7c834db297796d33ff5
James Kenyon <james.kenyon08@gmail.com>
2020-06-30 16:21:38 +0100

    Updated the IngredientAPIService to only work locally on the machine.

--- ios/Elenco/Elenco/Service/IngredientAPIService.swift
+++ ios/Elenco/Elenco/Service/IngredientAPIService.swift
@@ -10,13 +10,9 @@ import Foundation
 
 internal class IngredientAPIService {
     
-    // API Information
-    private static let autocompleteAddress = "https://api.spoonacular.com/food/ingredients/autocomplete"
-    private static let apiKey = "85c4130102284fe0ad261a1cb5dd5551"
-    
-    // save [IngredientName:Ingredient]
+    // save all the ingredients
     private static var ingredientCache: [String: Ingredient] = [:]
-    // save [IngredientSearch: Ingredients]
+    // save ingredient search results
     private static var ingredientListCache: [String: Ingredients] = [:]
     // save the most recent api request query
     private static var mostRecentAPIQuery: String = ""
@@ -42,7 +38,7 @@ internal class IngredientAPIService {
                         }
                     }
                 } catch {
-                    print("Error")
+                    return // don't handle error
                 }
             }
         }
@@ -54,39 +50,25 @@ internal class IngredientAPIService {
             query - the name that of the ingredient that the user is searching for
             numResults - the number of results to return
      */
-    static func getPossibleIngredientsFor(query: String, numResults: Int = 5, completion: @escaping (Ingredients)->()) {
+    static func getPossibleIngredientsFor(query: String, numResults: Int = 5) -> Ingredients {
+        var returnIngredients: Ingredients = []
         if let cachedList = ingredientListCache[query] {
-            completion(cachedList)
+            return cachedList
         } else {
-            if query.count >= 5 {
-                completion(getPossibleIngredientsForQueryFromCache(shortQuery: mostRecentAPIQuery, fullQuery: query))
-            } else {
-                let formattedQuery = query.replacingOccurrences(of: " ", with: "%20")
-                let url = "\(autocompleteAddress)?apiKey=\(apiKey)&query=\(formattedQuery)&number=\(numResults)&metaInformation=true"
-                guard let nsUrl = NSURL(string: url) else {
-                    completion([])
-                    return
+            for key in ingredientCache.keys {
+                if returnIngredients.count == numResults {
+                    break
                 }
-                let request = NSMutableURLRequest(url: nsUrl as URL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0)
-                let session = URLSession.shared
-                let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
-                    if (error != nil) {
-                        completion([])
-                    } else {
-                        if let data = data {
-                            if let ingredients = try? JSONDecoder().decode(Ingredients.self, from: data) {
-                                // save details to cache and memory before returning
-                                mostRecentAPIQuery = query
-                                ingredientListCache[query] = ingredients
-                                updateIngredientCache(ingredients: ingredients)
-                                completion(ingredients)
-                            }
-                        }
+                if key.contains(query.lowercased()) {
+                    if let ingredient = ingredientCache[key] {
+                        returnIngredients.append(ingredient)
                     }
-                })
-                dataTask.resume()
+                }
             }
+            mostRecentAPIQuery = query
+            ingredientListCache[query] = returnIngredients
         }
+        return returnIngredients
     }
     
     /*/
@@ -105,22 +87,6 @@ internal class IngredientAPIService {
     }
     
     // MARK: Private Interface
-    
-    /*
-     Limit the requests being sent to the api by searching through the cache.
-     */
-    private static func getPossibleIngredientsForQueryFromCache(shortQuery:String, fullQuery: String) -> Ingredients {
-        guard let ingredients = ingredientListCache[shortQuery] else {
-            return []
-        }
-        var returnIngredients: Ingredients = []
-        for ingredient in ingredients {
-            if ingredient.name.lowercased().contains(fullQuery.lowercased()) {
-                returnIngredients.append(ingredient)
-            }
-        }
-        return returnIngredients
-    }
     
     /*
      Save the new values to the ingredient cache
