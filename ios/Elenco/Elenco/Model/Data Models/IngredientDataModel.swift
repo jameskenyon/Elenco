0804c06c29f4aa2e5ec0c974b993e062c8f85020
James Kenyon <james.kenyon08@gmail.com>
2020-07-03 20:46:41 +0100

    Made the core data methods use a background thread so that operations occur in the background.

--- ios/Elenco/Elenco/Model/Data Models/IngredientDataModel.swift
+++ ios/Elenco/Elenco/Model/Data Models/IngredientDataModel.swift
@@ -24,61 +24,68 @@ class IngredientDataModel: ObservableObject {
     // MARK: - Fetch Methods
     
     // Fetch Ingredients
-    public func fetchIngredients(completion: (Error?)->()) {
-        let request: NSFetchRequest<IngredientStore> = IngredientStore.fetchRequest()
-        do {
-            let ingredientsEntities = try context.fetch(request)
-            self.ingredients = ingredientsEntities.map({ Ingredient(ingredientStore: $0) })
-            completion(nil)
-        } catch (let error) {
-            completion(error)
+    public func fetchIngredients(completion: @escaping (Error?)->()) {
+        DispatchQueue.global().async {
+            let request: NSFetchRequest<IngredientStore> = IngredientStore.fetchRequest()
+            do {
+                let ingredientsEntities = try self.context.fetch(request)
+                self.ingredients = ingredientsEntities.map({ Ingredient(ingredientStore: $0) })
+                completion(nil)
+            } catch (let error) {
+                completion(error)
+            }
         }
     }
     
     // MARK: - Update Methods
     
     // Save Ingredient to core data model
-    public func save(ingredient: Ingredient, completion: (Error?) -> ()) {
-        let ingredientStore = IngredientStore(context: context)
-        ingredientStore.name      = ingredient.name
-        ingredientStore.aisle     = ingredient.aisle
-        ingredientStore.quantity  = ingredient.quantity
-        ingredientStore.completed = ingredient.completed
+    public func save(ingredient: Ingredient, completion: @escaping (Error?) -> ()) {
+        DispatchQueue.global().async {
+            let ingredientStore = IngredientStore(context: self.context)
+            ingredientStore.name      = ingredient.name
+            ingredientStore.aisle     = ingredient.aisle
+            ingredientStore.quantity  = ingredient.quantity
+            ingredientStore.completed = ingredient.completed
 
-        do {
-            try context.save()
-            completion(nil)
-        } catch (let error) {
-            completion(error)
+            do {
+                try self.context.save()
+                completion(nil)
+            } catch (let error) {
+                completion(error)
+            }
         }
     }
     
     // Update the completion of a ingredient
-    public func update(ingredient: Ingredient, completion: (Error?)->()) {
-        let request: NSFetchRequest<IngredientStore> = IngredientStore.fetchRequest()
-        request.predicate = NSPredicate(format: "name == %@", ingredient.name)
-        
-        do {
-            guard let ingredientsEntity = try context.fetch(request).first else { return }
-            ingredientsEntity.setValue(ingredient.completed, forKey: "completed")
-            try context.save()
-            completion(nil)
-        } catch (let error) {
-            completion(error)
+    public func update(ingredient: Ingredient, completion: @escaping (Error?)->()) {
+        DispatchQueue.global().async {
+            let request: NSFetchRequest<IngredientStore> = IngredientStore.fetchRequest()
+            request.predicate = NSPredicate(format: "name == %@", ingredient.name)
+            do {
+                guard let ingredientsEntity = try self.context.fetch(request).first else { return }
+                ingredientsEntity.setValue(ingredient.completed, forKey: "completed")
+                try self.context.save()
+                completion(nil)
+            } catch (let error) {
+                completion(error)
+            }
         }
     }
     
     // Delete ingredient from data model
-    public func delete(ingredient: Ingredient, completion: (Error?)->()) {
-        let request: NSFetchRequest<IngredientStore> = IngredientStore.fetchRequest()
-        request.predicate = NSPredicate(format: "name == %@", ingredient.name)
-        do {
-            guard let ingredientsEntity = try context.fetch(request).first else { return }
-            context.delete(ingredientsEntity)
-            try context.save()
-            completion(nil)
-        } catch (let error) {
-            completion(error)
+    public func delete(ingredient: Ingredient, completion: @escaping (Error?)->()) {
+        DispatchQueue.global().async {
+            let request: NSFetchRequest<IngredientStore> = IngredientStore.fetchRequest()
+            request.predicate = NSPredicate(format: "name == %@", ingredient.name)
+            do {
+                guard let ingredientsEntity = try self.context.fetch(request).first else { return }
+                self.context.delete(ingredientsEntity)
+                try self.context.save()
+                completion(nil)
+            } catch (let error) {
+                completion(error)
+            }
         }
     }
 }
