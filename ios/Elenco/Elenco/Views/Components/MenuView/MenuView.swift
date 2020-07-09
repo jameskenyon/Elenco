24da2b724e03caaa93ab66c6c436acb0d2e8b420
jamesBernhardt4444 <james.bernhardt3@gmail.com>
2020-07-09 21:33:42 +0100

    Create MenuView file

---
+++ ios/Elenco/Elenco/Views/Components/MenuView/MenuView.swift
@@ -0,0 +1,23 @@
+//
+//  MenuView.swift
+//  Elenco
+//
+//  Created by James Bernhardt on 09/07/2020.
+//  Copyright © 2020 Elenco. All rights reserved.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    
+    var body: some View {
+        Text("Hello")
+    }
+    
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
