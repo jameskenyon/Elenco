7de1d1a366f0fb44f0899513ee0f8ac0c8a7d625
James Kenyon <james.kenyon08@gmail.com>
2020-07-09 21:31:57 +0100

    Added ElencoList model.

---
+++ ios/Elenco/Elenco/Models/ElencoList.swift
@@ -0,0 +1,37 @@
+//
+//  List.swift
+//  Elenco
+//
+//  Created by James Kenyon on 09/07/2020.
+//  Copyright © 2020 Elenco. All rights reserved.
+//
+
+import Foundation
+
+/*
+ 
+ The List class will hold an array of ingredients and
+ also model a general list in the app. The user is allowed to have
+ multiple lists which they can populate with ingredients.
+ 
+ */
+
+struct ElencoList: Codable, Identifiable, Hashable  {
+    
+    let name: String
+    let id: UUID
+    private(set) var ingredients: Ingredients
+    
+    let isSharedList: Bool = false
+    
+    init(name: String, id: UUID = UUID(), ingredients: Ingredients) {
+        self.name = name
+        self.id = id
+        self.ingredients = ingredients
+    }
+    
+    public mutating func updateIngredients(newIngredients: Ingredients) {
+        self.ingredients = newIngredients
+    }
+    
+}
