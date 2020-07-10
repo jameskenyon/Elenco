a42cd2a359bc9dc9b3044c714f0aaf2982b8aeec
jamesBernhardt4444 <james.bernhardt3@gmail.com>
2020-07-10 11:32:06 +0100

    Add title and underline view to menu view

--- ios/Elenco/Elenco/Views/Components/MenuView/MenuView.swift
+++ ios/Elenco/Elenco/Views/Components/MenuView/MenuView.swift
@@ -11,7 +11,47 @@ import SwiftUI
 struct MenuView: View {
     
     var body: some View {
-        Text("Hello")
+        ZStack(alignment: .leading) {
+            Rectangle()
+                .edgesIgnoringSafeArea(.all)
+            
+            
+            GeometryReader{ geometry in
+                Rectangle()
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(width: self.getWidth(geometry: geometry), height: geometry.size.height, alignment: .leading)
+                    .foregroundColor(Color.white)
+                    .padding(.trailing, self.getWidth(geometry: geometry) / 2)
+
+                    
+                .overlay(
+                    
+                    VStack(alignment: .leading) {
+                        Text("My Lists")
+                            .font(.system(size: 35, weight: .bold, design: .default))
+                            .foregroundColor(Color("Tungsten"))
+                            .padding(.leading, 20)
+                        
+                        Rectangle()
+                            .size(width: self.getWidth(geometry: geometry) - 80, height: 1)
+                            .foregroundColor(Color("Teal"))
+                            .padding(.leading, 20)
+                            .padding(.top, -5)
+                    }
+                    , alignment: .top)
+            }
+            
+            
+        }
+        
+        
+    
+        
+        
+    }
+    
+    private func getWidth(geometry: GeometryProxy) -> CGFloat {
+        return geometry.size.width - (geometry.size.width / 3)
     }
     
 }
