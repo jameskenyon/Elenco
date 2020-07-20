dc9dced4e5257c053876b301eadad0ad4bbd4fbb
jamesBernhardt4444 <james.bernhardt3@gmail.com>
2020-07-20 12:37:51 +0100

    Change tutorial titles when swiping between tutorials

---
+++ ios/Elenco/Elenco/Views/Components/TutorialView/ElencoPagerView.swift
@@ -0,0 +1,75 @@
+//
+//  ElencoPagerView.swift
+//  Elenco
+//
+//  Created by James Bernhardt on 20/07/2020.
+//  Copyright © 2020 Elenco. All rights reserved.
+//
+
+import SwiftUI
+
+struct ElencoPagerView<Content: View>: View {
+    let pageCount: Int
+    @Binding var currentIndex: Int
+    let content: Content
+    @GestureState private var translation: CGFloat = 0
+
+    init(pageCount: Int, currentIndex: Binding<Int>, @ViewBuilder content: () -> Content) {
+        self.pageCount = pageCount
+        self._currentIndex = currentIndex
+        self.content = content()
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                HStack(spacing: 0) {
+                    self.content.frame(width: geometry.size.width)
+                }
+                .frame(width: geometry.size.width, alignment: .leading)
+                .offset(x: -CGFloat(self.currentIndex) * geometry.size.width)
+                .offset(x: self.translation)
+                .animation(.spring())
+                    
+                .gesture(
+                    DragGesture()
+                        .updating(self.$translation) { value, state, _ in
+                        state = value.translation.width
+                    }
+                    .onEnded { value in
+                        let offset = value.translation.width / geometry.size.width
+                        let newIndex = (CGFloat(self.currentIndex) - offset).rounded()
+                        self.currentIndex = min(max(Int(newIndex), 0), self.pageCount - 1)
+                    }
+                )
+                
+                HStack {
+                    ForEach(0 ..< self.pageCount) { number in
+                        PagerIndicator(isActive: number == self.currentIndex)
+                    }
+                }
+                .padding(.bottom, 20)
+            }
+        }
+    }
+}
+
+// MARK: - Pager Indicator Circle
+struct PagerIndicator: View {
+    var isActive: Bool
+    var size: CGFloat = 15
+    
+    var body: some View {
+        ZStack {
+            if isActive {
+                Circle()
+                    .frame(width: size, height: size)
+                    .foregroundColor(Color("Light-Teal"))
+            } else {
+                Circle()
+                    .stroke(Color("Light-Teal"), lineWidth: 2)
+                    .frame(width: size, height: size)
+            }
+        }
+    }
+}
--- ios/Elenco/Elenco/Views/Components/TutorialView/TutorialView.swift
+++ ios/Elenco/Elenco/Views/Components/TutorialView/TutorialView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct TutorialView: View {
     
     @State var currentPage: Int = 0
+    @State var tutorialTitles = ["Edit Your Lists", "Smart Ingredient Adding", "Ingredient Prediction"]
     
     var body: some View {
         GeometryReader { geometry in
@@ -20,87 +21,22 @@ struct TutorialView: View {
                 
                 .overlay(
                     VStack(alignment: .center) {
-                        Text("Edit Your Lists")
+                        Text(self.tutorialTitles[self.currentPage])
                             .font(.system(size: 30, weight: .bold, design: .default))
                             .foregroundColor(Color("Lead"))
                             .padding(.top, 50)
+                            .animation(.default)
                         
                         Rectangle()
                             .foregroundColor(Color("Lead"))
                             .frame(width: geometry.size.width * 0.2, height: 1)
                         
-                        PagerView(pageCount: 2, currentIndex: self.$currentPage) {
+                        ElencoPagerView(pageCount: 2, currentIndex: self.$currentPage) {
                             EmptyListView()
                             ListMenuTutorialView()
                         }
                     }
                 , alignment: .top)
-        }
-    }
-    
-    // Return size based on percentage of screen size
-    private func size(percent: CGFloat, size: CGFloat) -> CGFloat {
-        return size * percent
-    }
-}
-
-struct PagerView<Content: View>: View {
-    let pageCount: Int
-    @Binding var currentIndex: Int
-    let content: Content
-    @GestureState private var translation: CGFloat = 0
-
-    init(pageCount: Int, currentIndex: Binding<Int>, @ViewBuilder content: () -> Content) {
-        self.pageCount = pageCount
-        self._currentIndex = currentIndex
-        self.content = content()
-    }
-    
-    var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                HStack(spacing: 0) {
-                    self.content.frame(width: geometry.size.width)
-                }
-                .frame(width: geometry.size.width, alignment: .leading)
-                .offset(x: -CGFloat(self.currentIndex) * geometry.size.width)
-                .offset(x: self.translation)
-                .animation(.spring())
-                .gesture(
-                    DragGesture().updating(self.$translation) { value, state, _ in
-                        state = value.translation.width
-                    }.onEnded { value in
-                        let offset = value.translation.width / geometry.size.width
-                        let newIndex = (CGFloat(self.currentIndex) - offset).rounded()
-                        self.currentIndex = min(max(Int(newIndex), 0), self.pageCount - 1)
-                    }
-                )
-                HStack {
-                    ForEach(0 ..< self.pageCount) { number in
-                        PagerIndicator(isActive: number == self.currentIndex)
-                    }
-                }
-                .padding(.bottom, 20)
-            }
-        }
-    }
-}
-
-struct PagerIndicator: View {
-    var isActive: Bool
-    var size: CGFloat = 15
-    
-    var body: some View {
-        ZStack {
-            if isActive {
-                Circle()
-                    .frame(width: size, height: size)
-                    .foregroundColor(Color("Light-Teal"))
-            } else {
-                Circle()
-                    .stroke(Color("Light-Teal"), lineWidth: 2)
-                    .frame(width: size, height: size)
-            }
         }
     }
 }
