8b3a589edde8982625d4384ddb36727d86efcd86
jameskenyon <james.kenyon08@gmail.com>
2020-07-20 10:54:44 +0100

    Created ElencoTextField which can be used across the app.
    As long as the view conforms to ElencoTextFieldDisplayable, the view is able to display an ElencoTextField.

---
+++ ios/Elenco/Elenco/Views/Components/ElencoTextField.swift
@@ -0,0 +1,84 @@
+//
+//  ElencoTextField.swift
+//  Elenco
+//
+//  Created by James Kenyon on 12/07/2020.
+//  Copyright © 2020 Elenco. All rights reserved.
+//
+
+import SwiftUI
+
+/*
+ 
+ Any view conforming to ElencoTextFieldDisplayable will be able to display
+ a text field in their view.
+ 
+ */
+protocol ElencoTextFieldDisplayable {
+    func userDidReturnOnTextField()
+    func userDidEditTextField(newValue: String)
+}
+
+/*
+ 
+ The Elenco Text Field will be responsible for displaying a more dynamic
+ text field in the app. This way we can use this text field throughout the app
+ for all of the text fields that we need.
+ 
+ */
+
+struct ElencoTextField: UIViewRepresentable {
+    
+    class Coordinator: NSObject, UITextFieldDelegate {
+
+        @Binding var text: String
+        var textFieldView: ElencoTextFieldDisplayable
+    
+        var didBecomeFirstResponder = false
+
+        init(text: Binding<String>, view: ElencoTextFieldDisplayable) {
+            _text = text
+            self.textFieldView = view
+        }
+        
+        func textFieldDidChangeSelection(_ textField: UITextField) {
+            if let newText = textField.text {
+                text = newText
+                self.textFieldView.userDidEditTextField(newValue: newText)
+            }
+        }
+        
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            self.textFieldView.userDidReturnOnTextField()
+        }
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            self.textFieldView.userDidReturnOnTextField()
+            return true
+        }
+
+    }
+
+    @Binding var text: String
+    
+    var isFirstResponder: Bool = false
+    var textFieldView: ElencoTextFieldDisplayable
+    
+    func makeUIView(context: UIViewRepresentableContext<ElencoTextField>) -> UITextField {
+        let textField = UITextField()
+        textField.delegate = context.coordinator
+        return textField
+    }
+
+    func makeCoordinator() -> ElencoTextField.Coordinator {
+        return Coordinator(text: $text, view: textFieldView)
+    }
+
+    func updateUIView(_ uiView: UITextField, context: UIViewRepresentableContext<ElencoTextField>) {
+        uiView.text = text
+        if isFirstResponder && !context.coordinator.didBecomeFirstResponder  {
+            uiView.becomeFirstResponder()
+            context.coordinator.didBecomeFirstResponder = true
+        }
+    }
+}
