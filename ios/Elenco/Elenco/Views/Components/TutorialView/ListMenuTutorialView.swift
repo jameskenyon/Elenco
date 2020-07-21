7a158bfde9d7e76d740e225d08ce137f04a28817
jamesBernhardt4444 <james.bernhardt3@gmail.com>
2020-07-21 12:54:06 +0100

    Custom menu view tutorial

--- ios/Elenco/Elenco/Views/Components/TutorialView/ListMenuTutorialView.swift
+++ ios/Elenco/Elenco/Views/Components/TutorialView/ListMenuTutorialView.swift
@@ -30,10 +30,46 @@ struct ListMenuTutorialView: View {
                     MenuHeaderView(title: "My Lists", width: geometry.size.width * 0.8)
                         .padding(.bottom, 10)
                     
-                    Image(uiImage: self.colorScheme == .dark ? #imageLiteral(resourceName: "menuTutorialDark") : #imageLiteral(resourceName: "menuTutorialLight"))
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: geometry.size.width * 0.8, height: geometry.size.width * 0.3)
+                    Rectangle()
+                        .overlay(
+                            VStack(alignment: .leading, spacing: 0) {
+                                ZStack {
+                                    Capsule()
+                                    .foregroundColor(Color(#colorLiteral(red: 0.368627451, green: 0.7883469462, blue: 0.6629261374, alpha: 1)))
+                                    .frame(width: geometry.size.width, height: 50)
+                                        .padding(.trailing, geometry.size.width * 0.2)
+                                    HStack {
+                                        Text("All")
+                                        .font(.custom("HelveticaNeue-Regular", size: 20))
+                                            .foregroundColor(Color.white)
+                                        
+                                        
+                                        Image(uiImage: #imageLiteral(resourceName: "editList"))
+                                            .resizable()
+                                            .foregroundColor(Color.white)
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 25, height: 25)
+                                        
+                                        Image(uiImage: #imageLiteral(resourceName: "deleteList"))
+                                            .resizable()
+                                            .foregroundColor(Color.white)
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 25, height: 25)
+                                    }
+                                }
+                                
+                                
+                                MenuViewListCell(list: ElencoList(name: "Christmas"), isEditing: false)
+                            }
+                            
+                        )
+                        .frame(width: geometry.size.width * 0.7, height: geometry.size.width / 4)
+                        .foregroundColor(Color.clear)
+                    .clipped()
+//                    Image(uiImage: self.colorScheme == .dark ? #imageLiteral(resourceName: "menuTutorialDark") : #imageLiteral(resourceName: "menuTutorialLight"))
+//                        .resizable()
+//                        .scaledToFit()
+//                        .frame(width: geometry.size.width * 0.6, height: geometry.size.width * 0.25)
                     
                     
                     Text("Edit a list or add a new one in my lists view ")
