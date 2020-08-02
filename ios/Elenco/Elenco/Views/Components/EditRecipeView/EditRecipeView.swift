a38e50800819a2e6d88b4d7b3d4a7de62908734e
jamesBernhardt4444 <james.bernhardt3@gmail.com>
2020-08-02 18:51:40 +0100

    Add placeholder to ElencoTextField for use in edit recipe view

--- ios/Elenco/Elenco/Views/Components/EditRecipeView/EditRecipeView.swift
+++ ios/Elenco/Elenco/Views/Components/EditRecipeView/EditRecipeView.swift
@@ -13,13 +13,21 @@ struct EditRecipeView: View {
     var recipe: Recipe?
     @State var recipeName: String = ""
     @State var time: String = ""
+    @State var isVegitarian: Bool = false
+    @State var isNutFree: Bool = false
+    @State var isGlutenFree: Bool = false
+    
+    init(recipe: Recipe?) {
+        UISwitch.appearance().onTintColor = #colorLiteral(red: 0.3696106672, green: 0.7883469462, blue: 0.6629261374, alpha: 1)
+        self.recipe = recipe
+    }
     
     var body: some View {
         GeometryReader { geometry in
             VStack(alignment: .leading) {
                 
                 HStack {
-                    RecipeEditField(fieldName: "Name", fieldText: self.$recipeName, size: geometry.size)
+                    RecipeEditField(fieldName: "Name", placeholder: "Recipe Name", fieldText: self.$recipeName, size: geometry.size)
                     
                     Spacer()
                     
@@ -50,7 +58,16 @@ struct EditRecipeView: View {
                     }
                 }
                 
-                RecipeEditField(fieldName: "Time", fieldText: self.$time, size: geometry.size)
+                RecipeEditField(fieldName: "Time", placeholder: "Time To Make", fieldText: self.$time, size: geometry.size)
+                
+                Text("Daily Requirements")
+                    .font(.custom("HelveticaNeue-Regular", size: 15))
+                    .foregroundColor(Color("Light-Gray"))
+                    .padding(.bottom)
+                
+                DietryToggle(dietryToggle: self.$isVegitarian, dietryName: "Vegitarian")
+                DietryToggle(dietryToggle: self.$isNutFree, dietryName: "Nut Free")
+                DietryToggle(dietryToggle: self.$isGlutenFree, dietryName: "Gluten Free")
                 
             }
             .padding(.horizontal, 30)
@@ -60,9 +77,29 @@ struct EditRecipeView: View {
     
 }
 
+struct DietryToggle: View {
+    
+    @Binding var dietryToggle: Bool
+    var dietryName: String
+    
+    var body: some View {
+        HStack {
+            Toggle("", isOn: $dietryToggle)
+            .labelsHidden()
+            
+            Text(dietryName)
+                .font(.custom("HelveticaNeue-Bold", size: 30))
+                .foregroundColor(dietryToggle ? Color("Lead") : Color("Light-Gray"))
+                .animation(.easeIn(duration: 5))
+                .padding(.leading)
+        }
+    }
+}
+
 struct RecipeEditField: View, ElencoTextFieldDisplayable {
     
     var fieldName: String
+    var placeholder: String
     @Binding var fieldText: String
     var size: CGSize
     
@@ -72,10 +109,10 @@ struct RecipeEditField: View, ElencoTextFieldDisplayable {
                 .font(.custom("HelveticaNeue-Regular", size: 15))
             .foregroundColor(Color("Light-Gray"))
             
-            ElencoTextField(text: $fieldText, isFirstResponder: false, textFieldView: self, font: UIFont(name: "HelveticaNeue-Bold", size: 25), color: UIColor.white)
+            ElencoTextField(text: $fieldText, isFirstResponder: false, textFieldView: self, font: UIFont(name: "HelveticaNeue-Bold", size: 25), color: #colorLiteral(red: 0.1298420429, green: 0.1298461258, blue: 0.1298439503, alpha: 1), placeholder: placeholder)
                 .textFieldStyle(PlainTextFieldStyle())
                 .foregroundColor(Color("Tungtsen"))
-                
+                .accentColor(Color("Teal"))
                 .frame(width: size.width * 0.6, height: 50)
         }
     }
@@ -93,6 +130,6 @@ struct RecipeEditField: View, ElencoTextFieldDisplayable {
 
 struct EditRecipeView_Previews: PreviewProvider {
     static var previews: some View {
-        EditRecipeView()
+        EditRecipeView(recipe: nil)
     }
 }
